c48a95db763bc08db7fe51f2450f46b620482473
fundon <cfddream@gmail.com>
2015-01-04 15:55:49 +0800

    host ping action

---
+++ src/js/services/host_actions.js
@@ -0,0 +1,23 @@
+(function () {
+'use strict';
+
+angular.module('dockerboard.services')
+  .factory('HostActions', ['$resource', function ($resource) {
+    var res = $resource('/api/hosts/:Id/:action', {
+      Id: '@Id',
+      action: '@action'
+    }, {
+      ping: {
+        method: 'GET',
+        params: {
+          action: 'ping'
+        },
+        transformResponse: function (data, headers) {
+          return { text: data };
+        },
+        isArray: false
+      }
+    });
+    return res;
+  }]);
+})();
